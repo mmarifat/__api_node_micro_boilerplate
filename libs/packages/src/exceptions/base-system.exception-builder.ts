7eca6e50874f5a5da843262c96bef8320bfa1551
mmarifat <mma.rifat66@gmail.com>
2023-03-23 12:03:45 +0000

    - status code and message extraction
    Signed-off-by: mmarifat <mma.rifat66@gmail.com>

--- libs/packages/src/exceptions/base-system.exception-builder.ts
+++ libs/packages/src/exceptions/base-system.exception-builder.ts
@@ -6,20 +6,25 @@ import SystemErrorDto = IoDtos.SystemErrorDto;
 import ResponseDto = IoDtos.ResponseDto;
 
 export function BaseSystemExceptionBuilder(error: any) {
+    let status = error.status || error.statusCode || error.httpCode || error.code || HttpStatus.INTERNAL_SERVER_ERROR;
+    let message =
+        error.message ||
+        error.errorMessage ||
+        error.errorDescription ||
+        error.error_message ||
+        error.error_description ||
+        'Internal Server Error';
+
+    if (error.isGuard) {
+        status = HttpStatus.FORBIDDEN;
+        message = error?.guardMessage || 'You are not authorized to access!';
+    }
+
     const fieldErrors = [];
     if (error.errors) {
         Object.keys(error.errors).forEach((key) => {
             fieldErrors.push(new FieldErrorDto(`${key}`, error.errors[key].value, error.errors[key].message));
         });
-    }
-    let status = HttpStatus.INTERNAL_SERVER_ERROR;
-    if (error.status) {
-        status = error.status;
-    }
-    let message = error.message ? error.message : 'Error';
-    if (error.isGuard) {
-        status = HttpStatus.FORBIDDEN;
-        message = error?.guardMessage || 'You are not authorized to access!';
     }
     let errorDto: ErrorDto;
     if (fieldErrors.length > 0) {
